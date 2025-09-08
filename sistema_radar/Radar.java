9f07aa8037d45678af7eae5a726799362ee60a7e
rai03-s <raissakat11@gmail.com>
2025-09-08 09:54:15 -0300

    add private

--- sistema_radar/Radar.java
+++ sistema_radar/Radar.java
@@ -2,8 +2,29 @@ package sistema_radar;
 
 public class Radar {
 
-    public String localizacao;
+    private String localizacao;
     public Integer limiteVelocidade;
+
+    public Radar(String localizacao, Integer limiteVelocidade){
+        this.localizacao = localizacao;
+        this.limiteVelocidade = limiteVelocidade;
+
+    }
+
+    public String getLocalizacao(){
+        return this.localizacao;
+    }
+    public void setLocalizacao(String localizacao){
+        this.localizacao = localizacao;
+    }
+
+    public Integer getLimiteVelocidade(){
+        return this.limiteVelocidade;
+    }
+
+    public void setLimiteVelocidade(Integer limiteVelocidade){
+        this.limiteVelocidade = limiteVelocidade;
+    }
 
     public void emitirNotificacao(String placa, Integer velocidadeObservada){
         System.out.println("Placa: "+placa);
@@ -12,8 +33,8 @@ public class Radar {
         System.out.println("Velocidade da via: "+this.limiteVelocidade);
     }
     public void avaliarVelocidade(Carro carro){
-        if (carro.velocidade > this.limiteVelocidade){
-            emitirNotificacao(carro.placa, carro.velocidade);
+        if (carro.getVelocidade() > this.limiteVelocidade){
+            emitirNotificacao(carro.getPlaca(), carro.getVelocidade());
             
         }
 
--- sistema_radar/Simulacao.java
+++ sistema_radar/Simulacao.java
@@ -2,33 +2,12 @@ package sistema_radar;
 
 public class Simulacao {
     public static void main(String[] args) {
-        Carro voyage = new Carro();
-        voyage.ano = 07;
-        voyage.modelo = "Comodoro";
-        voyage.placa = "AUZ3R13";
-        voyage.velocidade = 0;
         
-        Carro opala = new Carro();
-        opala.ano = 76;
-        opala.modelo = "Comodoro";
-        opala.placa = "JUS3T12";
-        opala.velocidade = 0;
+        Carro opala = new Carro("JUS3T12", 0, 76, "Comodoro");
 
-        Radar radar = new Radar();
-        radar.localizacao = "Pistão Sul";
-        radar.limiteVelocidade = 60;
+        Radar radar = new Radar("Pistão Sul", 60);
 
-        radar.avaliarVelocidade(voyage);
-
-        voyage.acelerar(); //10
-        voyage.acelerar();
-        voyage.acelerar();
-        voyage.acelerar();
-        voyage.acelerar();
-        voyage.acelerar();
-        voyage.acelerar();
-        voyage.acelerar(); //80
-        voyage.freiar();
+        radar.avaliarVelocidade(opala);
 
         opala.acelerar();
         opala.acelerar();
@@ -39,7 +18,6 @@ public class Simulacao {
         opala.acelerar();
         opala.acelerar();
 
-        radar.avaliarVelocidade(voyage);
         radar.avaliarVelocidade(opala);
     }
     
