b1020d7181b055dc08e5afa31d7e8efcc5af306d
rai03-s <raissakat11@gmail.com>
2025-09-16 01:23:56 -0300

    add corre

--- atividade1/Main.java
+++ atividade1/Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        Computador computador = new Computador(12, 500, 4, 3.5f);
+
+        Programas programa1 = new Programas(14, 450, 2, 3);
+        Programas programa2 = new Programas(8, 600, 2, 3);
+        Programas programa3 = new Programas(8, 300, 2, 2);
+
+        SistemaOperacional sistemaOperacional = new SistemaOperacional(computador);
+
+        System.out.println("Teste 1");
+        sistemaOperacional.executarPrograma(programa1);
+        
+    }
     
 }
--- atividade1/SistemaOperacional.java
+++ atividade1/SistemaOperacional.java
@@ -1,23 +1,27 @@
-public class SistemaOperacional extends Computador {
+public class SistemaOperacional {
+    private Computador computador;
 
-    private Boolean executarPrograma;
-
-    public SistemaOperacional(Boolean executarPrograma) {
-        this.executarPrograma = executarPrograma;
+	public SistemaOperacional(Computador computador) {
     }
 
-    public SistemaOperacional(){
-        super(getMemoriaRAM(), getSSD(), getNucleos(), getOperacoesPorSegundo())
-    }
-    public Boolean isExecutarPrograma() {
-        return this.executarPrograma;
+    public Boolean executarPrograma(Programa p){
+        if (p.getSSDOcupado() > computador.getSSD()) {
+            System.out.println("Erro de instalação: SSD insuficiente.");
+            return false;    
+        }
+        if (p.getMemoriaRAMAlocada()> computador.getMemoriaRAM()) {
+            System.out.println("Erro de instalação: Memória Insuficiente.");
+            return false;
+        }
+        Integer tempoExecucao = calcularTempoExecucao(p);
+        System.out.println("Tempo de execução: " + tempoExecucao + " segundos.");
+        return true;
     }
 
-    public Boolean getExecutarPrograma() {
-        return this.executarPrograma;
+        private Integer calcularTempoExecucao(Programa p){
+            float operacoesPorSegundo = computador.getOperacoesPorSegundo();
+            Integer nucleos = computador.getNucleos();
+            Integer quantidadeOperacoes = p.getQuantidadeOperacoes();
+            return (int) (quantidadeOperacoes/ (operacoesPorSegundo*nucleos));
+      }
     }
-
-    public void setExecutarPrograma(Boolean executarPrograma) {
-        this.executarPrograma = executarPrograma;
-    }
-}
