15508a2b79a73a98e4eff42bf0d7ac1cbb2db7ad
rai03-s <raissakat11@gmail.com>
2025-10-13 00:16:29 -0300

    Update SecretariaFazenda.java

--- sistema_iptu/SecretariaFazenda.java
+++ sistema_iptu/SecretariaFazenda.java
@@ -4,9 +4,10 @@ public class SecretariaFazenda {
     public static void main(String[] args) {
         Municipio brasilia = new Municipio("Brasília", "DF", 29.50);
 
-        Chacara chacara = new Chacara(brasilia, 3000.0,6 , false);
+        Chacara chacara = new Chacara(brasilia, 3000.0, 6 , false);
         Apartamento apt = new Apartamento(brasilia, 63.0, 1, true);
         System.out.println("Chácara IPTU R$: " + chacara.calcularIPTU());
         System.out.println("Apt IPTU R$: " + apt.calcularIPTU());
     }
 }
+
